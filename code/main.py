0747e6db17acfe9ecb2f2edf664be64baeb90029
Harald Vinje <haraldvinje@gmail.com>
2018-10-04 17:30:46 +0200

    Split up database and http requests. MVP Crud now working

--- code/main.py
+++ code/main.py
@@ -1,69 +1,75 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from form_collection import FormCollection
 
 
 app = Flask(__name__)
 
-
-#Running mongoDB on local host. Requires mongoDB to be installed and run locally. Must be generalized/dockerized
-
-client = MongoClient('mongodb://datastore:27017/dockerdemo')
-
-"""
-app.config['MONGO_DBNAME'] = 'form'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/form'
-
-mongo = PyMongo(app)
-"""
-
-db = client.cidevdb
-
-form_collection = db.form
+form_collection = FormCollection()
 
 
-@app.route('/')
-def hello_docker():
-	form_collection.insert_one({'name': 'TreeCuttingForm'})
-	return 'Hello friends! I have just inserted a form'
-
-@app.route('/get_forms', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_all_forms():
-    form = form_collection 
+    
+    forms = form_collection.get_all_forms()
+    
+    for form in forms:
+        del form['_id']
 
-    output = []
+    return jsonify({'all_forms' : forms})
 
-    for q in form.find():
-        output.append({'name': q['name']})
+@app.route('/{id:}', methods=['GET'])
+def get_one_form(id):
+    query = request.get_json()
+    name = query['name']
+    author = query['author']
+    form = form_collection.get_one_form(name, author)
 
-    return jsonify({'result' : output})
+    return jsonify({'result' : form})
 
-@app.route('/form/<name>', methods=['GET'])
-def get_one_form(name):
-    form = form_collection
-
-    q = form.find_one({'name' : name})
-
-    if q:
-        output = q
-    else:
-        output = 'No results found'
-
-    return jsonify({'result' : output})
-
-@app.route('/form', methods=['POST'])
+@app.route('/', methods=['POST'])
 def add_form():
-    form = form_collection 
 
-    name = request.json['name']
+    form = request.get_json()
+    form_collection.add_form(form)
 
-    form_id = form.insert_one({'name' : name})
-    new_form = form.find_one({'_id' : form_id})
+    return jsonify({'message': 'successfully inserted document'})
 
-    output = {'name' : new_form['name']}
 
-    return jsonify({'result' : output})
+@app.route('/{id}', methods=['PUT'])
+def update_one_form(id):
 
+    form = request.get_json()
+    name = form['name']
+    author = form['author']
+
+    form_collection.update_one_form()
+
+    return jsonify({'message': 'successfully updated document'})
+
+
+
+@app.route('/{}', methods=['DELETE'])
+def delete_one_form():
+
+    form = request.get_json()
+    name = form['name']
+    author = form['author']
+
+    form_collection.delete_one_form(name, author)    
+
+    return jsonify({'message': 'successfully deleted document'})
+"""
+#dangerous to have "delete all" as an option
+
+@app.route('/delete_all_forms', methods=['POST'])
+def delete_all_forms():
+    form_collection.delete_all_forms()
+    return jsonify({'message': 'successfully deleted all forms'})
+"""
 
 # Only for testing purposes - should use WSGI server in production
 if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0', port=8080)
+   
