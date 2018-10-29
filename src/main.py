c92367b0284f857aedcd60fe5f2882b16dee707f
Harald Vinje <haraldvinje@gmail.com>
2018-10-29 20:33:19 +0100

    clean up

--- src/main.py
+++ src/main.py
@@ -10,66 +10,70 @@ form_collection = FormCollection()
 
 @app.route('/', methods=['GET'])
 def get_all_forms():
-    
     forms = form_collection.get_all_forms()
-    
+
     for form in forms:
         del form['_id']
 
     return jsonify({'all_forms' : forms})
 
-@app.route('/{id:}', methods=['GET'])
-def get_one_form(id):
-    query = request.get_json()
-    name = query['name']
-    author = query['author']
-    form = form_collection.get_one_form(name, author)
 
-    return jsonify({'result' : form})
+@app.route('/<int:id>', methods=['GET'])
+def get_one_form(id):
+    
+    if id<0:
+        return jsonify({'message': 'invalid id'}), 401
+    try:
+        form = form_collection.get_form_by_id(id)
+
+        return jsonify({'form': form})
+    except ValueError:
+        return jsonify({'message': 'form does not exist'}), 404
 
 @app.route('/', methods=['POST'])
 def add_form():
+    try:
+        form = request.get_json()
+        #TODO: additional checks for required parameters in json object
+        form_collection.add_form(form)
+        return jsonify({'message': 'hello! Successfully inserted document'}), 201
 
-    form = request.get_json()
-    form_collection.add_form(form)
+    except:
+        return jsonify({'message': 'Credentials not provied'}), 401
 
-    return jsonify({'message': 'successfully inserted document'})
-
-
-@app.route('/{id}', methods=['PUT'])
+@app.route('/<int:id>', methods=['PUT'])
 def update_one_form(id):
+ 
+    if id<0:
+        return jsonify({'message': 'invalid id'}), 401
+    try:
+        updates = request.get_json()
+        form_collection.update_one_form(id, updates)
+        print("the id is", id)
+        return jsonify({'message': 'Successfully updated doc'}), 200
 
-    form = request.get_json()
-    name = form['name']
-    author = form['author']
-
-    form_collection.update_one_form()
-
-    return jsonify({'message': 'successfully updated document'})
+    except ValueError:
+        return jsonify({'message': 'form does not exist'}), 404
 
 
 
-@app.route('/{}', methods=['DELETE'])
-def delete_one_form():
+@app.route('/<int:id>', methods=['DELETE'])
+def delete_one_form(id):
 
-    form = request.get_json()
-    name = form['name']
-    author = form['author']
+    if id<0:
+        return jsonify({'message': 'invalid id'}), 401
+    try:
+        form_collection.delete_form_by_id(id)
 
-    form_collection.delete_one_form(name, author)    
+        return jsonify({'message': 'form successfully deleted'}), 200
+    except ValueError:
+        return jsonify({'message': 'form does not exist'}), 404
 
-    return jsonify({'message': 'successfully deleted document'})
-"""
-#dangerous to have "delete all" as an option
 
-@app.route('/delete_all_forms', methods=['POST'])
-def delete_all_forms():
-    form_collection.delete_all_forms()
-    return jsonify({'message': 'successfully deleted all forms'})
-"""
-
-# Only for testing purposes - should use WSGI server in production
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', port=8080)
-   
+ 
+
+
+
